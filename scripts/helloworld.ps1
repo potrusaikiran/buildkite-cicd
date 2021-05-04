704ebdb461329d017f2fc3e62da1f4848576517c
saikiran <saikiran.potru@gmail.com>
2021-05-04 19:54:17 +0530

    added parameters section

--- scripts/helloworld.ps1
+++ scripts/helloworld.ps1
@@ -1,10 +1,15 @@
 param (
-    [Parameter(Mandatory = $true)][string]$rg,
-    [Parameter(Mandatory = $true)][string]$location
+  [Parameter(Mandatory = $true)][string]$rg,
+  [Parameter(Mandatory = $true)][string]$location,
+  [Parameter(Mandatory = $true)][string]$SubscriptionId,
+  [Parameter(Mandatory = $true)][string]$tenantid,
+  [Parameter(Mandatory = $true)][string]$serviceprincipalid,
+  [Parameter(Mandatory = $true)][securestring]$serviceprincipalsecret
 )
 
 #$rg = 'buildkite-rg'
 #$location = 'Central India'
+Write-Output '$rg-$location-$SubscriptionId-$tenantid-$serviceprincipalid'
 Import-Module -Name Az -Force
 $AzModule = (Get-Module Az -ErrorAction SilentlyContinue).Version
 if (!(($AzModule.Major -ge 5 -and $AzModule.Minor -ge 0 -and $AzModule.Build -ge 0))) {
@@ -12,8 +17,8 @@ if (!(($AzModule.Major -ge 5 -and $AzModule.Minor -ge 0 -and $AzModule.Build -ge
 }
 Write-output "=============================="
 Write-output "Hello buildkite from powershell"
-# $cred = New-Object System.Management.Automation.PSCredential($serviceprincipalid, ($serviceprincipal | ConvertTo-SecureString -AsPlainText -Force))
-# Login-AzAccount -Credential $cred -ServicePrincipal -Subscription $SubscriptionId -Tenant $tenantid
+$cred = New-Object System.Management.Automation.PSCredential($serviceprincipalid, ($serviceprincipalsecret | ConvertTo-SecureString -AsPlainText -Force))
+Login-AzAccount -Credential $cred -ServicePrincipal -Subscription $SubscriptionId -Tenant $tenantid
 
 Get-AzContext
 $buildkiterge = Get-AzResourceGroup -Name $rg -ErrorAction SilentlyContinue
