59da2be13397164fb728d6d1a7d9ced509992ee5
saikiran <saikiran.potru@gmail.com>
2021-05-04 12:52:59 +0530

    relative path reflection

--- scripts/helloworld.ps1
+++ scripts/helloworld.ps1
@@ -1,23 +1,21 @@
-$rg='buildkite-rg'
-$location='Central India'
+$rg = 'buildkite-rg'
+$location = 'Central India'
 Import-Module -Name Az -Force
 $AzModule = (Get-Module Az -ErrorAction SilentlyContinue).Version
 if (!(($AzModule.Major -ge 5 -and $AzModule.Minor -ge 0 -and $AzModule.Build -ge 0))) {
-    throw "Please install Azure PowerShell version 5.0.0"
+  throw "Please install Azure PowerShell version 5.0.0"
 }
 Write-output "=============================="
 Write-output "Hello buildkite from powershell"
-Connect-AzAccount
+#Connect-AzAccount
 Get-AzContext
-$buildkiterge=Get-AzResourceGroup -Name $rg -ErrorAction SilentlyContinue
-if(!$buildkiterge)
-{
+$buildkiterge = Get-AzResourceGroup -Name $rg -ErrorAction SilentlyContinue
+if (!$buildkiterge) {
   New-AzResourceGroup -Name $rg -Location $location -verbose
   write-output "Resource Group ($rg) created....."
 }
-else
-{
+else {
   write-output "Resource Group ($rg) exists....."
 }
-New-AzResourceGroupDeployment -ResourceGroupName $rg -Name 'stoage-deployment' -TemplateFile './templates\StorageAccount.json' -Verbose
+New-AzResourceGroupDeployment -ResourceGroupName $rg -Name 'stoage-deployment' -TemplateFile '.\templates\StorageAccount.json' -Verbose
 Write-output "====== Thank You just to check no offense nor credential ====="
