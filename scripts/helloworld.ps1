4c7246fafac5fb1c71955e2f48037f8a49d72dd7
Saikiran Potru <saikiran.potru@gmail.com>
2021-05-12 12:12:26 +0530

    Update helloworld.ps1

--- scripts/helloworld.ps1
+++ scripts/helloworld.ps1
@@ -1,15 +1,17 @@
-param (
-  [Parameter(Mandatory = $true)][string]$rg,
-  [Parameter(Mandatory = $true)][string]$location,
-  [Parameter(Mandatory = $true)][string]$SubscriptionId,
-  [Parameter(Mandatory = $true)][string]$tenantid,
-  [Parameter(Mandatory = $true)][string]$serviceprincipalid,
-  [Parameter(Mandatory = $true)][string]$serviceprincipalsecret
+Param(
+	[Parameter(Mandatory = $false)]
+	[string] $serviceprincipal,
+	[Parameter(Mandatory = $false)]
+	[string] $serviceprincipalid,
+	[Parameter(Mandatory = $false)]
+	[string] $SubscriptionId,
+	[Parameter(Mandatory = $false)]
+	[string] $tenantid,
+    [Parameter(Mandatory = $true)][string]$rg,
+    [Parameter(Mandatory = $true)][string]$location
 )
 
-#$rg = 'buildkite-rg'
-#$location = 'Central India'
-Write-Output '$rg-$location-$SubscriptionId-$tenantid-$serviceprincipalid'
+
 Import-Module -Name Az -Force
 $AzModule = (Get-Module Az -ErrorAction SilentlyContinue).Version
 if (!(($AzModule.Major -ge 5 -and $AzModule.Minor -ge 0 -and $AzModule.Build -ge 0))) {
@@ -19,9 +21,9 @@ Write-output "=============================="
 Write-output "Hello buildkite from powershell"
 Clear-AzContext -Force
 
-$cred = New-Object System.Management.Automation.PSCredential($serviceprincipalid, ($serviceprincipalsecret | ConvertTo-SecureString -AsPlainText -Force))
-Login-AzAccount -Credential $cred -ServicePrincipal -Subscription $SubscriptionId -Tenant $tenantid
-Set-AzContext -Subscription $SubscriptionId -Tenant $tenantid -Force
+$cred = New-Object System.Management.Automation.PSCredential($serviceprincipalid, ($serviceprincipal | ConvertTo-SecureString -AsPlainText -Force))
+Connect-AzAccount -Credential $cred -ServicePrincipal -Subscription $SubscriptionId -Tenant $tenantid
+Select-AzSubscription -Subscription $SubscriptionId
 $buildkiterge = Get-AzResourceGroup -Name $rg -ErrorAction SilentlyContinue
 if (!$buildkiterge) {
   New-AzResourceGroup -Name $rg -Location $location -verbose
@@ -30,5 +32,6 @@ if (!$buildkiterge) {
 else {
   write-output "Resource Group ($rg) exists....."
 }
+
 New-AzResourceGroupDeployment -ResourceGroupName $rg -Name 'stoage-deployment' -TemplateFile '.\templates\StorageAccount.json' -Verbose
 Write-output "====== Thank You just to check no offense nor credential ====="
